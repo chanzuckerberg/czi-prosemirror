a78e6bc47cefc6b38ce6dc2a54b586f4198029c6
Hedger Wang <hedgerwang+github@gmail.com>
2018-11-26 12:41:55 -0800

    build dist files.

---
+++ dist/EditorState.js
@@ -0,0 +1,11 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _prosemirrorState = require('prosemirror-state');
+
+var ProseMirrorEditorState = _prosemirrorState.EditorState;
+
+exports.default = ProseMirrorEditorState;
---
+++ dist/EditorState.js.flow
@@ -0,0 +1,7 @@
+// @flow
+
+import {EditorState} from 'prosemirror-state';
+
+const ProseMirrorEditorState = EditorState;
+
+export default ProseMirrorEditorState;
