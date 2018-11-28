e3aa44f5dce3e7dbaef4690844823ead448d9f32
Hedger Wang <hedgerwang+github@gmail.com>
2018-11-28 15:32:37 -0800

    mock image upload API in demo app.

--- demo/DemoAppRuntime.js
+++ demo/DemoAppRuntime.js
@@ -2,8 +2,11 @@
 
 // This implements the interface of `EditorRuntime`.
 
+import type {ImageLike} from '../src/Types';
+
 class DemoAppRuntime {
 
+  // Image Proxy
   canProxyImageSrc(): boolean {
     return true;
   }
@@ -11,6 +14,21 @@ class DemoAppRuntime {
   getProxyImageSrc(src: string): string {
     // This simulate a fake proxy.
     return src + '#proxied=' + Date.now();
+  }
+
+  // Image Upload
+  canUploadImage(): boolean {
+    return true;
+  }
+
+  uploadImage(blob: Object): Promise<ImageLike> {
+    const img: ImageLike = {
+      id: '',
+      width: 200,
+      height: 300,
+      src: 'https://placekitten.com/200/300',
+    };
+    return Promise.resolve(img);
   }
 }
 
