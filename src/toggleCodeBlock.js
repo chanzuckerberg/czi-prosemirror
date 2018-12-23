0ba8072bdd5682279dee9f24092cad1a1298c1dd
Hedger Wang <hedgerwang+github@gmail.com>
2018-12-23 15:12:21 -0800

    Clear marks of code block.

--- src/toggleCodeBlock.js
+++ src/toggleCodeBlock.js
@@ -1,9 +1,12 @@
 // @flow
 
 import {Schema} from 'prosemirror-model';
+import {TextSelection} from 'prosemirror-state';
 import {Transform} from 'prosemirror-transform';
 
-import {CODE_BLOCK, PARAGRAPH} from './NodeNames';
+import {MARK_LINK} from './MarkNames';
+import {BLOCKQUOTE, CODE_BLOCK, HEADING, PARAGRAPH} from './NodeNames';
+import clearMarks from './clearMarks';
 import compareNumber from './compareNumber';
 import isListNode from './isListNode';
 
@@ -15,24 +18,37 @@ export default function toggleCodeBlock(
   const {selection, doc} = tr;
   const codeBlock = nodes[CODE_BLOCK];
   const paragraph = nodes[PARAGRAPH];
+  const heading = nodes[HEADING];
+  const blockquote = nodes[BLOCKQUOTE];
   if (!selection || !doc || !codeBlock || !paragraph) {
     return tr;
   }
 
   const poses = [];
   const {from, to} = tr.selection;
+  let allowed = true;
   let startWithCodeBlock = null;
   doc.nodesBetween(from, to, (node, pos) => {
     const nodeType = node.type;
     if (startWithCodeBlock === null) {
       startWithCodeBlock = nodeType === codeBlock;
     }
-    poses.push(pos);
-    return !isListNode(node);
+    const {type, isBlock} = node;
+    if (isBlock) {
+      allowed = allowed && (
+        type === paragraph ||
+        type === codeBlock ||
+        type === heading ||
+        type === blockquote
+      );
+      allowed && poses.push(pos);
+    }
+
+    return isBlock;
   });
 
   // Update from the bottom to avoid disruptive changes in pos.
-  poses.sort(compareNumber).reverse().forEach(pos => {
+  allowed && poses.sort(compareNumber).reverse().forEach(pos => {
     tr = setCodeBlockNodeEnabled(
       tr,
       schema,
@@ -65,14 +81,24 @@ function setCodeBlockNodeEnabled(
   const {nodes} = schema;
   const codeBlock = nodes[CODE_BLOCK];
   const paragraph = nodes[PARAGRAPH];
-  if (!enabled && paragraph && node.type === codeBlock) {
+
+  if (codeBlock && !enabled && node.type === codeBlock) {
     tr = tr.setNodeMarkup(
       pos,
       paragraph,
       node.attrs,
       node.marks,
     );
-  } else if (enabled && codeBlock && node.type === paragraph) {
+  } else if (enabled && node.type !== codeBlock) {
+    const {selection} = tr;
+    tr = tr.setSelection(TextSelection.create(
+      tr.doc,
+      pos,
+      pos + node.nodeSize,
+    ));
+    tr = clearMarks(tr, schema);
+    tr = tr.removeMark(pos, pos + node.nodeSize, schema.marks[MARK_LINK]);
+    tr = tr.setSelection(selection);
     tr = tr.setNodeMarkup(
       pos,
       codeBlock,
