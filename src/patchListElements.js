6699afac91bb0895f072d0b63550e12b21e312c0
Hedger Wang <hedgerwang+github@gmail.com>
2019-01-17 17:20:35 -0800

    support line-height and padding for <li />.

--- src/patchListElements.js
+++ src/patchListElements.js
@@ -1,7 +1,7 @@
 // @flow
 
 import {ATTRIBUTE_LIST_STYLE_COLOR, ATTRIBUTE_LIST_STYLE_TYPE} from './ListItemNodeSpec';
-import {ATTRIBUTE_INDENT, convertMarginLeftToIndentValue} from './ParagraphNodeSpec';
+import {ATTRIBUTE_INDENT, EMPTY_CSS_VALUE, convertMarginLeftToIndentValue} from './ParagraphNodeSpec';
 import {ATTRIBUTE_CSS_BEFORE_CONTENT} from './patchStyleElements';
 import toHexColor from './ui/toHexColor';
 
@@ -14,6 +14,7 @@ const CHAR_BULLET = '\u25cf';
 const CHAR_CIRCLE = '\u25cb';
 const CHAR_SQUARE = '\u25a0';
 const CHAR_BOX = '\u274f';
+const INLINE_NODE_NAME_PATTERN = /^(#text)|(A|SPAN|B|STRONG)$/;
 
 function patchListElementsElement(listElement: HTMLElement): void {
   // If the children of `listElement` all have teh same marginLeft, assume
@@ -25,8 +26,10 @@ function patchListElementsElement(listElement: HTMLElement): void {
     // TODO: Handle this later.
     console.error('nested list is not supported', listElement);
   }
-  Array.from(children).some(listItemElement => {
+  Array.from(children).forEach(listItemElement => {
     const {style} = listItemElement;
+    patchPaddingStyle(listItemElement);
+
     const bc = listItemElement.getAttribute(ATTRIBUTE_CSS_BEFORE_CONTENT) || '';
     if (beforeContent === undefined) {
       beforeContent = bc;
@@ -39,10 +42,10 @@ function patchListElementsElement(listElement: HTMLElement): void {
     if (marginLeft === undefined) {
       marginLeft = ml;
     }
+
     if (ml !== marginLeft) {
       marginLeft = null;
     }
-
 
     const {firstElementChild, lastElementChild} = listItemElement;
     if (firstElementChild && firstElementChild === lastElementChild) {
@@ -107,5 +110,43 @@ function patchListElementsElement(listElement: HTMLElement): void {
     if (listStyleType) {
       listElement.setAttribute(ATTRIBUTE_LIST_STYLE_TYPE, listStyleType);
     }
+  }
+}
+
+// This moves the styles of <li /> into its content <p />.
+function patchPaddingStyle(listItemElement: HTMLElement): void {
+  const {style, childNodes} = listItemElement;
+  const {paddingTop, paddingBottom, lineHeight} = style;
+  if (
+    !EMPTY_CSS_VALUE.has(paddingBottom) &&
+    !EMPTY_CSS_VALUE.has(paddingTop) &&
+    !EMPTY_CSS_VALUE.has(lineHeight)
+  ) {
+    return;
+  }
+
+  const doc = listItemElement.ownerDocument;
+  const frag = doc.createDocumentFragment();
+  let contentIsInline = true;
+
+  Array.from(childNodes).forEach(cn => {
+    contentIsInline = contentIsInline &&
+      INLINE_NODE_NAME_PATTERN.test(cn.nodeName);
+    frag.appendChild(cn);
+  });
+
+  if (contentIsInline) {
+    // Wrap all inline content with <p /> with the padding style applied.
+    const pEl = doc.createElement('p');
+    Object.assign(pEl.style, {
+      lineHeight,
+      paddingBottom,
+      paddingTop,
+    });
+    pEl.appendChild(frag);
+    listItemElement.appendChild(pEl);
+  } else {
+    // Unable to patch the style.
+    listItemElement.appendChild(frag);
   }
 }
