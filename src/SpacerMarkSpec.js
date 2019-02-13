6c2c5afaa85dba9ff4dfe1af775535069d881490
Hedger Wang <hedgerwang+github@gmail.com>
2019-02-13 15:57:37 -0800

    Add `SpacerMarkSpec` to render tab space.

---
+++ src/SpacerMarkSpec.js
@@ -0,0 +1,43 @@
+// @flow
+
+import {Node} from 'prosemirror-model';
+
+import type {MarkSpec} from './Types';
+
+export const DOM_ATTRIBUTE_SIZE = 'data-spacer-size';
+export const SPACER_SIZE_TAB = 'tab';
+
+const SpacerMarkSpec: MarkSpec = {
+  attrs: {
+    size: {default: SPACER_SIZE_TAB},
+  },
+  defining: true,
+  draggable: false,
+  excludes: '_',
+  group: 'inline',
+  inclusive: false,
+  inline: true,
+  spanning: false,
+  parseDOM: [
+    {
+      tag: 'span[data-spacer-size]',
+      getAttrs: (el) => {
+        return {
+           size: el.getAttribute(DOM_ATTRIBUTE_SIZE) || SPACER_SIZE_TAB,
+        };
+      },
+    },
+  ],
+  toDOM(node: Node) {
+    const {size} = node.attrs;
+    return [
+      'span',
+      {
+        [DOM_ATTRIBUTE_SIZE]: size,
+      },
+      0,
+    ];
+  },
+};
+
+export default SpacerMarkSpec;
