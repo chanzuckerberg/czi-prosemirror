9215ae57f629aabeb6372629f45879f4c04e17f8
Vicky <velveret@users.noreply.github.com>
2019-03-04 11:10:19 -0800

    [fix] Swap 10 and 11 in font size menu. (#13)

--- src/ui/FontSizeCommandMenuButton.js
+++ src/ui/FontSizeCommandMenuButton.js
@@ -9,7 +9,7 @@ import FontSizeCommand from '../FontSizeCommand';
 import CommandMenuButton from './CommandMenuButton';
 import findActiveFontSize from './findActiveFontSize';
 
-const FONT_PT_SIZES = [8, 9, 11, 10, 12, 14, 18, 24, 30, 36, 48, 60, 72, 90];
+const FONT_PT_SIZES = [8, 9, 10, 11, 12, 14, 18, 24, 30, 36, 48, 60, 72, 90];
 
 const FONT_PT_SIZE_COMMANDS = FONT_PT_SIZES.reduce((memo, size) => {
   memo[` ${size} `] = new FontSizeCommand(size);
