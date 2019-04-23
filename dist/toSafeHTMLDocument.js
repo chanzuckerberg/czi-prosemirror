30da3f59f3283f27f1147c64672e1ef09c51ddf2
Vicky Lai <velveret@users.noreply.github.com>
2019-04-23 16:31:19 -0700

    Update comment

--- dist/toSafeHTMLDocument.js
+++ dist/toSafeHTMLDocument.js
@@ -7,8 +7,7 @@ exports.default = toSafeHTMLDocument;
 
 
 // Parses HTML in a detached document to help with avoiding XSS
-// https://developer.mozilla.org/en-US/docs/Web/API/DOMImplementation.createHTMLDocument
-// https://developer.mozilla.org/en-US/Add-ons/Code_snippets/HTML_to_DOM
+// https://developer.mozilla.org/en-US/docs/Archive/Add-ons/Code_snippets/HTML_to_DOM
 // https://github.com/ProseMirror/prosemirror/issues/473#issuecomment-255727531
 function toSafeHTMLDocument(html) {
   var parser = new window.DOMParser();
