a8ac7bf00b3ce4fee4e4192440ae6420d1231e22
Hedger Wang <hedgerwang+github@gmail.com>
2019-05-02 23:05:52 -0700

    rename variable.

--- dist/isTextStyleMarkCommandEnabled.js
+++ dist/isTextStyleMarkCommandEnabled.js
@@ -22,7 +22,7 @@ var _MarkNames = require('./MarkNames');
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-var ValidMathTextMarkNames = new _set2.default([_MarkNames.MARK_FONT_SIZE, _MarkNames.MARK_TEXT_COLOR]);
+var VALID_MATH_MARK_NAMES = new _set2.default([_MarkNames.MARK_FONT_SIZE, _MarkNames.MARK_TEXT_COLOR]);
 
 // Whether the command for apply specific text style mark is enabled.
 
@@ -37,7 +37,7 @@ function isTextStyleMarkCommandEnabled(state, markName) {
     return false;
   }
   var mathNodeType = schema.nodes[_NodeNames.MATH];
-  if (mathNodeType && ValidMathTextMarkNames.has(markName) && (0, _isNodeSelectionForNodeType2.default)(selection, mathNodeType)) {
+  if (mathNodeType && VALID_MATH_MARK_NAMES.has(markName) && (0, _isNodeSelectionForNodeType2.default)(selection, mathNodeType)) {
     // A math node is selected.
     return true;
   }
