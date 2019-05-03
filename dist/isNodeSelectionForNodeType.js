1c2c64f31b1a381ec8ce2f19aa4850e14a9d8278
Hedger Wang <hedgerwang+github@gmail.com>
2019-05-03 13:37:23 -0700

    build dist files.

--- dist/isNodeSelectionForNodeType.js
+++ dist/isNodeSelectionForNodeType.js
@@ -9,7 +9,7 @@ var _prosemirrorState = require('prosemirror-state');
 
 var _prosemirrorModel = require('prosemirror-model');
 
-// Whether the seletion is a node for the node type provided.
+// Whether the selection is a node for the node type provided.
 function isNodeSelectionForNodeType(selection, nodeType) {
   if (selection instanceof _prosemirrorState.NodeSelection) {
     return selection.node.type === nodeType;
--- dist/isNodeSelectionForNodeType.js.flow
+++ dist/isNodeSelectionForNodeType.js.flow
@@ -3,7 +3,7 @@
 import {Selection, NodeSelection} from 'prosemirror-state';
 import {NodeType} from 'prosemirror-model';
 
-// Whether the seletion is a node for the node type provided.
+// Whether the selection is a node for the node type provided.
 export default function isNodeSelectionForNodeType(
   selection: Selection,
   nodeType: NodeType
