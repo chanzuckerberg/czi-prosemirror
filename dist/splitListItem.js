40d36acc46e3ab0fa0974bcf296173995fc150eb
Hedger Wang <hedgerwang+github@gmail.com>
2019-05-29 10:16:07 -0700

    split list item.

--- dist/splitListItem.js
+++ dist/splitListItem.js
@@ -3,13 +3,24 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
+
+var _extends2 = require('babel-runtime/helpers/extends');
+
+var _extends3 = _interopRequireDefault(_extends2);
+
 exports.default = splitListItem;
 
 var _prosemirrorModel = require('prosemirror-model');
 
+var _prosemirrorState = require('prosemirror-state');
+
 var _prosemirrorTransform = require('prosemirror-transform');
 
 var _NodeNames = require('./NodeNames');
+
+var _prosemirrorUtils = require('prosemirror-utils');
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 // Build a command that splits a non-empty textblock at the top level
 // of a list item by also splitting that list item.
@@ -43,25 +54,33 @@ function splitListItem(tr, schema) {
     // In an empty block. If this is a nested list, the wrapping
     // list item should be split. Otherwise, bail out and let next
     // command handle lifting.
-    if ($from.depth == 2 || $from.node(-3).type !== nodeType || $from.index(-2) != $from.node(-2).childCount - 1) {
-      return tr;
-    }
+    // if (
+    //   $from.depth == 2 ||
+    //   $from.node(-3).type !== nodeType ||
+    //   $from.index(-2) != $from.node(-2).childCount - 1
+    // ) {
+    //   return tr;
+    // }
 
-    var wrap = _prosemirrorModel.Fragment.empty;
-    var keepItem = $from.index(-1) > 0;
-    // Build a fragment containing empty versions of the structure
-    // from the outer list item to the parent node of the cursor
-    for (var d = $from.depth - (keepItem ? 1 : 2); d >= $from.depth - 3; d--) {
-      wrap = _prosemirrorModel.Fragment.from($from.node(d).copy(wrap));
-    }
+    // let wrap = Fragment.empty;
+    // const keepItem = $from.index(-1) > 0;
+    // // Build a fragment containing empty versions of the structure
+    // // from the outer list item to the parent node of the cursor
+    // for (let d = $from.depth - (keepItem ? 1 : 2); d >= $from.depth - 3; d--) {
+    //   wrap = Fragment.from($from.node(d).copy(wrap));
+    // }
 
-    // Add a second list item with an empty default start node
-    wrap = wrap.append(_prosemirrorModel.Fragment.from(nodeType.createAndFill()));
-    tr = tr.replace($from.before(keepItem ? null : -1), $from.after(-3), new _prosemirrorModel.Slice(wrap, keepItem ? 3 : 2, 2));
+    // // Add a second list item with an empty default start node
+    // wrap = wrap.append(Fragment.from(nodeType.createAndFill()));
+    // tr = tr.replace(
+    //   $from.before(keepItem ? null : -1),
+    //   $from.after(-3),
+    //   new Slice(wrap, keepItem ? 3 : 2, 2)
+    // );
 
-    var pos = $from.pos + (keepItem ? 3 : 2);
-    tr = tr.setSelection(selection.constructor.near(tr.doc.resolve(pos)));
-    return tr;
+    // const pos = $from.pos + (keepItem ? 3 : 2);
+    // tr = tr.setSelection(selection.constructor.near(tr.doc.resolve(pos)));
+    return insertParagraphAndSplitBlankListItem(tr, schema);
   }
 
   var nextType = $to.pos == $from.end() ? grandParent.contentMatchAt($from.indexAfter(-1)).defaultType : null;
@@ -73,4 +92,58 @@ function splitListItem(tr, schema) {
   }
 
   return tr.split($from.pos, 2, types);
+}
+
+function insertParagraphAndSplitBlankListItem(tr, schema) {
+  var listItemType = schema.nodes[_NodeNames.LIST_ITEM];
+  var orderedListType = schema.nodes[_NodeNames.ORDERED_LIST];
+  var bulletListType = schema.nodes[_NodeNames.BULLET_LIST];
+  var paragraphType = schema.nodes[_NodeNames.PARAGRAPH];
+  if (!listItemType || !paragraphType) {
+    // Schema does not support the nodes expected.
+    return tr;
+  }
+  var listItemFound = (0, _prosemirrorUtils.findParentNodeOfType)(listItemType)(tr.selection);
+  if (!listItemFound || listItemFound.node.textContent !== '') {
+    // Selection cursor is not inside an empty list item.
+    return tr;
+  }
+
+  var listFound = orderedListType && (0, _prosemirrorUtils.findParentNodeOfType)(orderedListType)(tr.selection) || bulletListType && (0, _prosemirrorUtils.findParentNodeOfType)(bulletListType)(tr.selection);
+
+  if (!listFound) {
+    // Selection isn't inside an list.
+    return tr;
+  }
+
+  var $listItemPos = tr.doc.resolve(listItemFound.pos);
+  var listItemIndex = $listItemPos.index($listItemPos.depth);
+  if (listFound.node.childCount < 3 || listItemIndex < 1 || listItemIndex >= listFound.node.childCount - 1) {
+    // - The list must have at least three list items
+    // - The cursor must be after the first list item and before the last list
+    //   item.
+    // If both conditions don't match, bails out.
+    return tr;
+  }
+
+  var sliceFrom = listItemFound.pos + listItemFound.node.nodeSize;
+  var sliceTo = listFound.pos + listFound.node.nodeSize - 1;
+  var slicedItems = tr.doc.slice(sliceFrom, sliceTo, false);
+
+  var deleteFrom = listItemFound.pos;
+  var deleteTo = listFound.pos + listFound.node.nodeSize;
+  tr = tr.delete(deleteFrom, deleteTo);
+  var sourceListNode = listFound.node;
+  var listAttrs = (0, _extends3.default)({}, sourceListNode.attrs);
+  if (orderedListType === sourceListNode.type) {
+    listAttrs.counterReset = 'none';
+  }
+
+  var insertFrom = deleteFrom + 1;
+  var listNode = sourceListNode.type.create(listAttrs, slicedItems.content);
+  tr = tr.insert(insertFrom, listNode);
+  var paragraph = paragraphType.create({}, _prosemirrorModel.Fragment.empty);
+  tr = tr.insert(insertFrom, paragraph);
+  tr = tr.setSelection(_prosemirrorState.TextSelection.create(tr.doc, insertFrom));
+  return tr;
 }
--- dist/splitListItem.js.flow
+++ dist/splitListItem.js.flow
@@ -1,9 +1,10 @@
 // @flow
 
-import {Fragment, Schema, Slice} from 'prosemirror-model';
+import {Fragment, Schema} from 'prosemirror-model';
+import {TextSelection} from 'prosemirror-state';
 import {Transform, canSplit} from 'prosemirror-transform';
-
-import {LIST_ITEM} from './NodeNames';
+import {LIST_ITEM, ORDERED_LIST, BULLET_LIST, PARAGRAPH} from './NodeNames';
+import {findParentNodeOfType} from 'prosemirror-utils';
 
 // Build a command that splits a non-empty textblock at the top level
 // of a list item by also splitting that list item.
@@ -35,33 +36,33 @@ export default function splitListItem(
     // In an empty block. If this is a nested list, the wrapping
     // list item should be split. Otherwise, bail out and let next
     // command handle lifting.
-    if (
-      $from.depth == 2 ||
-      $from.node(-3).type !== nodeType ||
-      $from.index(-2) != $from.node(-2).childCount - 1
-    ) {
-      return tr;
-    }
+    // if (
+    //   $from.depth == 2 ||
+    //   $from.node(-3).type !== nodeType ||
+    //   $from.index(-2) != $from.node(-2).childCount - 1
+    // ) {
+    //   return tr;
+    // }
 
-    let wrap = Fragment.empty;
-    const keepItem = $from.index(-1) > 0;
-    // Build a fragment containing empty versions of the structure
-    // from the outer list item to the parent node of the cursor
-    for (let d = $from.depth - (keepItem ? 1 : 2); d >= $from.depth - 3; d--) {
-      wrap = Fragment.from($from.node(d).copy(wrap));
-    }
+    // let wrap = Fragment.empty;
+    // const keepItem = $from.index(-1) > 0;
+    // // Build a fragment containing empty versions of the structure
+    // // from the outer list item to the parent node of the cursor
+    // for (let d = $from.depth - (keepItem ? 1 : 2); d >= $from.depth - 3; d--) {
+    //   wrap = Fragment.from($from.node(d).copy(wrap));
+    // }
 
-    // Add a second list item with an empty default start node
-    wrap = wrap.append(Fragment.from(nodeType.createAndFill()));
-    tr = tr.replace(
-      $from.before(keepItem ? null : -1),
-      $from.after(-3),
-      new Slice(wrap, keepItem ? 3 : 2, 2)
-    );
+    // // Add a second list item with an empty default start node
+    // wrap = wrap.append(Fragment.from(nodeType.createAndFill()));
+    // tr = tr.replace(
+    //   $from.before(keepItem ? null : -1),
+    //   $from.after(-3),
+    //   new Slice(wrap, keepItem ? 3 : 2, 2)
+    // );
 
-    const pos = $from.pos + (keepItem ? 3 : 2);
-    tr = tr.setSelection(selection.constructor.near(tr.doc.resolve(pos)));
-    return tr;
+    // const pos = $from.pos + (keepItem ? 3 : 2);
+    // tr = tr.setSelection(selection.constructor.near(tr.doc.resolve(pos)));
+    return insertParagraphAndSplitBlankListItem(tr, schema);
   }
 
   const nextType =
@@ -76,4 +77,67 @@ export default function splitListItem(
   }
 
   return tr.split($from.pos, 2, types);
+}
+
+function insertParagraphAndSplitBlankListItem(
+  tr: Transform,
+  schema: Schema
+): Transform {
+  const listItemType = schema.nodes[LIST_ITEM];
+  const orderedListType = schema.nodes[ORDERED_LIST];
+  const bulletListType = schema.nodes[BULLET_LIST];
+  const paragraphType = schema.nodes[PARAGRAPH];
+  if (!listItemType || !paragraphType) {
+    // Schema does not support the nodes expected.
+    return tr;
+  }
+  const listItemFound = findParentNodeOfType(listItemType)(tr.selection);
+  if (!listItemFound || listItemFound.node.textContent !== '') {
+    // Selection cursor is not inside an empty list item.
+    return tr;
+  }
+
+  const listFound =
+    (orderedListType && findParentNodeOfType(orderedListType)(tr.selection)) ||
+    (bulletListType && findParentNodeOfType(bulletListType)(tr.selection));
+
+  if (!listFound) {
+    // Selection isn't inside an list.
+    return tr;
+  }
+
+  const $listItemPos = tr.doc.resolve(listItemFound.pos);
+  const listItemIndex = $listItemPos.index($listItemPos.depth);
+  if (
+    listFound.node.childCount < 3 ||
+    listItemIndex < 1 ||
+    listItemIndex >= listFound.node.childCount - 1
+  ) {
+    // - The list must have at least three list items
+    // - The cursor must be after the first list item and before the last list
+    //   item.
+    // If both conditions don't match, bails out.
+    return tr;
+  }
+
+  const sliceFrom = listItemFound.pos + listItemFound.node.nodeSize;
+  const sliceTo = listFound.pos + listFound.node.nodeSize - 1;
+  const slicedItems = tr.doc.slice(sliceFrom, sliceTo, false);
+
+  const deleteFrom = listItemFound.pos;
+  const deleteTo = listFound.pos + listFound.node.nodeSize;
+  tr = tr.delete(deleteFrom, deleteTo);
+  const sourceListNode = listFound.node;
+  const listAttrs = {...sourceListNode.attrs};
+  if (orderedListType === sourceListNode.type) {
+    listAttrs.counterReset = 'none';
+  }
+
+  const insertFrom = deleteFrom + 1;
+  const listNode = sourceListNode.type.create(listAttrs, slicedItems.content);
+  tr = tr.insert(insertFrom, listNode);
+  const paragraph = paragraphType.create({}, Fragment.empty);
+  tr = tr.insert(insertFrom, paragraph);
+  tr = tr.setSelection(TextSelection.create(tr.doc, insertFrom));
+  return tr;
 }
