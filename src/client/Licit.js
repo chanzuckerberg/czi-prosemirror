883cff49d7ab75184b5efefc54ba508d2b757682
firmusoft <praveen.kumar@firmusoft.co.in>
2020-06-16 19:21:33 +0530

    Added LICIT properties:      *  width {string} [100%] Width of the editor.      *  height {height} [100%] Height of the editor.      *  readOnly {boolean} [false] To enable/disable editing mode.      *  onChange {@callback} [null] Fires after each significant change.      *      @param state {EditorState}      *      @param transaction {Transform}      *  data {JSON} [null] Document data to be loaded into the editor.

--- src/client/Licit.js
+++ src/client/Licit.js
@@ -6,7 +6,7 @@ import { Transform } from 'prosemirror-transform';
 import { EditorView } from 'prosemirror-view';
 import React from 'react';
 
-import createEmptyEditorState from '../createEmptyEditorState';
+import convertFromJSON from '../convertFromJSON';
 import RichTextEditor from '../ui/RichTextEditor';
 import uuid from '../uuid';
 import LicitRuntime from './LicitRuntime';
@@ -15,11 +15,28 @@ import SimpleConnector from './SimpleConnector';
 
 import './licit.css';
 
+/**
+ * LICIT properties:
+ *  docID {number} [1] Collaborative Doument ID
+ *  debug {boolean} [false] To enable/disable ProseMirror Debug Tools
+ *  collaborative {boolean} [false] To enable/disable Collborative editing mode.
+ *  width {string} [100%] Width of the editor.
+ *  height {height} [100%] Height of the editor.
+ *  readOnly {boolean} [false] To enable/disable editing mode.
+ *  onChange {@callback} [null] Fires after each significant change. 
+ *      @param state {EditorState} 
+ *      @param transaction {Transform}
+ *  data {JSON} [null] Document data to be loaded into the editor.
+ */
 class Licit extends React.PureComponent<any, any, any> {
   _runtime: any;
   _connector: any;
   _clientID: string;
   _debug: boolean;
+  _width: string;
+  _height: string;
+  _onChangeCB: any;
+  _readOnly: boolean;
 
   constructor(props: any, context: any) {
     super(props, context);
@@ -32,8 +49,12 @@ class Licit extends React.PureComponent<any, any, any> {
     const docID = props.docID || 1; // This is used only if collaborative.
     const COLLAB_EDITING = props.collaborative || false;
     this._debug = props.debug || false;
+    this._width = props.width || '100%';
+    this._height = props.height || '100%';
+    this._onChangeCB = props.onChange || null;
+    this._readOnly = props.readOnly || false;
 
-    const editorState = createEmptyEditorState();
+    const editorState = convertFromJSON(props.data);
 
     const setState = this.setState.bind(this);
     this._connector = COLLAB_EDITING ?
@@ -47,7 +68,6 @@ class Licit extends React.PureComponent<any, any, any> {
 
   render(): React.Element<any> {
     const {editorState} = this.state;
-    const readOnly = /read/ig.test(window.location.search);
     // [FS] IRAD-978 2020-06-05
     // Using 100vw & 100vh (100% viewport) is not ideal for a component which is expected to be a part of a page, 
     // so changing it to 100%  width & height which will occupy the area relative to its parent.	
@@ -55,12 +75,12 @@ class Licit extends React.PureComponent<any, any, any> {
       <RichTextEditor
         editorState={editorState}
         embedded={false}
-        height="100%"
+        height={this._height}
         onChange={this._onChange}
         onReady={this._onReady}
-        readOnly={readOnly}
+        readOnly={this._readOnly}
         runtime={this._runtime}
-        width="100%"
+        width={this._width}
       />
     );
   }
@@ -68,6 +88,9 @@ class Licit extends React.PureComponent<any, any, any> {
   _onChange = (data: {state: EditorState, transaction: Transform}): void => {
     const {transaction} = data;
     this._connector.onEdit(transaction);
+    if(this._onChangeCB) {
+      this._onChangeCB(data.state, data.transaction);
+    }
   };
 
   _onReady = (editorView: EditorView): void => {
