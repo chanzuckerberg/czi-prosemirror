1ea8c926abbbd5425a0f077a2cce8cfc9a9b84b5
seybi.ea <seybi.ea@firmusoft.co.in>
2020-09-04 17:04:04 +0530

    Fix:Collab fails due to unwanted semicolon in collabRequest method.

--- licit/server/collab/server.js
+++ licit/server/collab/server.js
@@ -23,7 +23,7 @@ function handleCollabRequest(req: any, resp: any) {
   initEditorSchema(effectiveSchema);
   const headers = {
     'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Methods': ';POST, GET, PUT, DELETE, OPTIONS',
+    'Access-Control-Allow-Methods': 'POST, GET, PUT, DELETE, OPTIONS',
     'Access-Control-Allow-Credential': false,
     'Access-Control-Max-Age': 86400, // 24hrs
     'Access-Control-Allow-Headers':
