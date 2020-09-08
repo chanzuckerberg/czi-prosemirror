2b67ea47be73cb1c188633b8a698a1a306811d0f
seybi.ea <seybi.ea@firmusoft.co.in>
2020-09-08 14:47:31 +0530

    Fix: Image flickering issues on keystroke

--- src/ui/resolveImage.js
+++ src/ui/resolveImage.js
@@ -18,7 +18,7 @@ const queue = [];
 
 export default function resolveImage(src: ?string): Promise<ImageResult> {
   return new Promise((resolve, reject) => {
-    const bag = {src, resolve, reject};
+    const bag = { src, resolve, reject };
     queue.push(bag);
     processQueue();
   });
@@ -63,7 +63,7 @@ function processPromise(
   const parsedURL = url.parse(srcStr);
   // [FS] IRAD-1007 2020-07-13
   // Removed the port validation from here
-  const {protocol} = parsedURL;
+  const { protocol } = parsedURL;
   if (!/(http:|https:|data:)/.test(protocol || window.location.protocol)) {
     resolve(result);
     return;
@@ -81,10 +81,11 @@ function processPromise(
     }
     resolve(result);
     dispose();
-   // [FS] IRAD-1006 2020-07-17
-   // Fix: Inconsistent behavior on image load
-   // Removed caching cache[srcStr] = {...result};
-    
+    // [FS] IRAD-1006 2020-07-17
+    // Fix: Inconsistent behavior on image load
+    // Avoid image caching remove the below line
+    cache[srcStr] = { ...result };
+
   };
 
   const onError = () => {
@@ -105,7 +106,7 @@ function processPromise(
     processQueue();
   };
 
-  const {body} = document;
+  const { body } = document;
   if (body) {
     img = document.createElement('img');
     img.style.cssText =
